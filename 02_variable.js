8de4f28ef850853257ad1798dbe4d0459ea917f4
Talaviya Ronak <168280442+Ronak501@users.noreply.github.com>
2024-06-12 20:06:09 +0000

    add variable file of javascript

---
+++ 02_variable.js
@@ -0,0 +1,9 @@
+const accountId = 134646
+
+let accountemail = "ronak@gmail.com"
+
+var accountPassword = "12334"
+
+//don't use var : scope issue
+
+console.table([accountId, accountPassword, accountemail])
---
+++ 03_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"  //use newr version
+
+// alert("Ronak")
+
+let name = "Ronak"
+
+let isLogin = true
+
+console.log(typeof name);
+console.log(typeof null);
